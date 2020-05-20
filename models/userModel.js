f779ff4b513a5c12e576079c2d1fd4bc7af85f09
Arindam Aluni <aaluni@gmail.com>
2020-05-20 17:01:41 +0100

    Implement bcrypt encryption

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -30,8 +31,18 @@ const userSchema = new mongoose.Schema({
       validator: function (el) {
         return el === this.password;
       },
+      message: "Password don't match with the confirmation password",
     },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified()) next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  console.log(this);
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
